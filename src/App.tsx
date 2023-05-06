b39b1cbd707c0ec52b7981d1c1b3856677435ef6
Dmitrii <dimau777@gmail.com>
2023-05-06 15:18:44 +0400

    feat: Working map with objects in plain HTML and JS

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,18 @@
 import React from 'react';
 import './App.css';
+import { Map } from "./Map/Map";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/*<header className="App-header">*/}
+      {/*  <p>*/}
+      {/*    Edit <code>src/App.tsx</code> and save to reload.*/}
+      {/*  </p>*/}
+      {/*</header>*/}
+      {/*<main>*/}
+      {/*  /!*<Map />*!/*/}
+      {/*</main>*/}
     </div>
   );
 }
