ad8763085e75a7a445ee24c581bab72ce3100cc7
Dmitrii <dimau777@gmail.com>
2023-05-06 16:36:09 +0400

    feat: Working full-page map based on react-leaflet

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
 import React from 'react';
-import './App.css';
 import { Map } from "./Map/Map";
 
 function App() {
   return (
     <div className="App">
-      {/*<header className="App-header">*/}
-      {/*  <p>*/}
-      {/*    Edit <code>src/App.tsx</code> and save to reload.*/}
-      {/*  </p>*/}
-      {/*</header>*/}
-      {/*<main>*/}
-      {/*  /!*<Map />*!/*/}
-      {/*</main>*/}
+      <main>
+        <Map />
+      </main>
     </div>
   );
 }
--- src/Map/Map.tsx
+++ src/Map/Map.tsx
@@ -1,8 +1,23 @@
 import React from "react";
 import styles from "./Map.module.css";
+import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
 
 function Map() {
-  return <div id={"map"} className={styles.root}></div>
+  return (
+      <div className={styles.root}>
+        <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+          <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[51.505, -0.09]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+      </div>
+  );
 }
 
 export { Map };
